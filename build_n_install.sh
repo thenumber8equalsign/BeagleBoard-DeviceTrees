11abe02a971d75fdcf510304c9f03307e44b66d8
Robert Nelson <robertcnelson@gmail.com>
2024-06-22 18:46:06 -0500

    build_n_install: ask for sudo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_n_install.sh
+++ build_n_install.sh
@@ -8,7 +8,12 @@ if [ -f /boot/firmware/extlinux/extlinux.conf ] ; then
 		soc_family=$(cat /sys/devices/soc0/family || true)
 		case "${soc_family}" in
 		AM62X|J721E|J722S)
-			sudo make install_arm64
+			if ! id | grep -q root; then
+				make install_arm64
+			else
+				echo "Installing Device-Tree's password required for sudo..."
+				sudo make install_arm64
+			fi
 			;;
 		esac
 	fi
