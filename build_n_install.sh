3ad66f99c5f6c250be36c028253b0ee4cde52b26
Robert Nelson <robertcnelson@gmail.com>
2025-04-24 18:29:34 -0500

    add: am335x-boneblack-revd
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- build_n_install.sh
+++ build_n_install.sh
@@ -3,11 +3,11 @@
 if [ -f /boot/firmware/extlinux/extlinux.conf ] ; then
 	if [ -f /sys/devices/soc0/family ] ; then
 		make clean
-		make -j4 all_arm64
-		echo "DeviceTrees: Built"
 		soc_family=$(cat /sys/devices/soc0/family || true)
 		case "${soc_family}" in
 		AM62X|J721E|J722S)
+			make -j4 all_arm64
+			echo "DeviceTrees: Built"
 			if ! id | grep -q root; then
 				echo "DeviceTrees: Install, Password required for sudo..."
 				sudo make install_arm64
@@ -24,7 +24,26 @@ if [ -f /boot/firmware/extlinux/extlinux.conf ] ; then
 		echo "DeviceTrees: Built"
 	fi
 else
-	make clean
-	make -j4
-	echo "DeviceTrees: Built"
+	if [ -f /sys/devices/soc0/family ] ; then
+		make clean
+		soc_family=$(cat /sys/devices/soc0/family || true)
+		case "${soc_family}" in
+		AM33xx)
+			make -j4 all_arm
+			echo "DeviceTrees: Built"
+			if ! id | grep -q root; then
+				echo "DeviceTrees: Install, Password required for sudo..."
+				sudo make install_arm
+				echo "DeviceTrees: Installed"
+			else
+				make install_arm
+				echo "DeviceTrees: Installed"
+			fi
+			;;
+		esac
+	else
+		make clean
+		make -j4
+		echo "DeviceTrees: Built"
+	fi
 fi
