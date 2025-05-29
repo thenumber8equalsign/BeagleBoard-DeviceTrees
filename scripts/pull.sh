e7b57c56eaa2579b131dfd2920f39b48f1f3acdd
Robert Nelson <robertcnelson@gmail.com>
2025-05-29 18:28:30 -0500

    sync: v6.15.x
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git v6.12.x
+git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git v6.12.x
+
