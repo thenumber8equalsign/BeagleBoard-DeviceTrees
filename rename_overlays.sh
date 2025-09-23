a77ba7955f628f5035a1d1e6f9bc5fab50eb8ad7
Robert Nelson <robertcnelson@gmail.com>
2025-09-23 10:51:46 -0500

    ci: move device trees under ti/omap
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- rename_overlays.sh
+++ rename_overlays.sh
@@ -3,3 +3,8 @@
 cd ./src/arm/overlays/
 rename 's/\.dts$/\.dtso/' *.dts
 cd -
+git add ./src/arm/overlays/ || true
+mkdir -p ./src/arm/ti/omap/
+mv -v ./src/arm/*.dts ./src/arm/ti/omap/ || true
+mv -v ./src/arm/*.dtsi ./src/arm/ti/omap/ || true
+git add ./src/arm/ || true
