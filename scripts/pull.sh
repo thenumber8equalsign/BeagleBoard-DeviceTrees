0bfbf4c5b289be1d329c61c2ff5d3ec3b2c63fa1
Robert Nelson <robertcnelson@gmail.com>
2025-10-29 09:31:51 -0500

    RevD overlays
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git v6.1.x
+git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git v6.1.x
+
