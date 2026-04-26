9634b4d6206a48350e20d1371458e2090ac30cdc
Robert Nelson <robertcnelson@gmail.com>
2026-04-26 18:38:02 -0500

    ci: update scripts/pull.sh
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git v6.19.x
-git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git v6.19.x
+git pull --no-edit https://github.com/beagleboard/BeagleBoard-DeviceTrees.git v7.0.x
+git pull --no-edit https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git v7.0.x
 
